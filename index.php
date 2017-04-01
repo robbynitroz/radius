0b04de28262795fe00dad18c7fedec3671cb9697
Tigran <haytiko@gmail.com>
2017-04-01 23:05:58 +0400

    first commit

--- index.php
+++ index.php
@@ -7,33 +7,13 @@ $fb = new Facebook\Facebook([
 		'app_secret' => 'f7c94fe5f0f51cc9a04fc2512b5c58cd',
 		'default_graph_version' => 'v2.8',
 ]);
+
 $helper = $fb->getRedirectLoginHelper();
+$permissions = ['email', 'user_likes']; // optional
+$loginUrl = $helper->getLoginUrl('http://{your-website}/login-callback.php', $permissions);
 
-try {
-	$accessToken = $helper->getAccessToken();
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	// When Graph returns an error
-	echo 'Graph returned an error: ' . $e->getMessage();
-	exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;
-}
-
-if (! isset($accessToken)) {
-	if ($helper->getError()) {
-		header('HTTP/1.0 401 Unauthorized');
-		echo "Error: " . $helper->getError() . "\n";
-		echo "Error Code: " . $helper->getErrorCode() . "\n";
-		echo "Error Reason: " . $helper->getErrorReason() . "\n";
-		echo "Error Description: " . $helper->getErrorDescription() . "\n";
-	} else {
-		header('HTTP/1.0 400 Bad Request');
-		echo 'Bad request';
-	}
-	exit;
-}
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+exit;
 
 
 /**
