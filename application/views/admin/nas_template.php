8221527e10621f89cf5c88c9499ae28667181cd0
Tigran <haytiko@gmail.com>
2017-04-01 21:03:52 +0400

    first commit

--- application/views/admin/nas_template.php
+++ application/views/admin/nas_template.php
@@ -83,22 +83,17 @@
                 console.log(response);
             });
 
+            FB.login(function(response) {
+                // handle the response
+            }, {scope: 'email,user_likes'});
+
             FB.Event.subscribe('auth.statusChange', function(response) {
                 alert('The status of the session is: ' + response.status);
             });
 
-            FB.api({
-                    method:     "pages.isFan",
-                    page_id:        my_page_id,
-                },  function(response) {
-                    console.log(response);
-                    if(response){
-                        alert('You Likey');
-                    } else {
-                        alert('You not Likey :(');
-                    }
-                }
-            );
+            FB.api('/me/likes/696113500523537', function(response) {
+                console.log(response.data);
+            });
 
         });
     });
