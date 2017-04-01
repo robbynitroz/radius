5da980604845ef9ec517381af61ed0a09f12f3b8
Tigran <haytiko@gmail.com>
2017-04-01 23:17:03 +0400

    first commit

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $fb = new Facebook\Facebook([
 
 $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email', 'user_likes']; // optional
-$loginUrl = $helper->getLoginUrl('http://{your-website}/login-callback.php', $permissions);
+$loginUrl = $helper->getLoginUrl('http://radiusdev.guestcompass.nl/index.php', $permissions);
 
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 exit;
