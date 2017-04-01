b30c94942d324971245984f69d1e06a64beb12ed
Tigran <haytiko@gmail.com>
2017-04-01 22:35:50 +0400

    first commit

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
 
+require './login/Facebook/autoload.php';
+
+$fb = new Facebook\Facebook([
+		'app_id' => '696113500523537',
+		'app_secret' => 'f7c94fe5f0f51cc9a04fc2512b5c58cd',
+		'default_graph_version' => 'v2.8',
+]);
+
+$helper = $fb->getJavaScriptHelper();
+
+try {
+	$accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	// When Graph returns an error
+	echo 'Graph returned an error: ' . $e->getMessage();
+//    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+//    exit;
+}
+
+if (! isset($accessToken)) {
+	echo 'No cookie set or no OAuth data could be obtained from cookie.';
+//    exit;
+}
+
+
 /**
  * CodeIgniter
  *
