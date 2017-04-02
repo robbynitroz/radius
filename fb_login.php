0ae0df5683cf9849d6d84b9efcb651ea8014d5cf
Tigran <haytiko@gmail.com>
2017-04-02 19:00:35 +0400

    first commit

--- fb_login.php
+++ fb_login.php
@@ -1,23 +1,5 @@
 <?php
-//session_start();
-//require '../login/Facebook/autoload.php';
-//
-//$fb = new Facebook\Facebook([
-//    'app_id' => '696113500523537',
-//    'app_secret' => 'f7c94fe5f0f51cc9a04fc2512b5c58cd',
-//    'default_graph_version' => 'v2.8',
-//]);
-//
-//$helper = $fb->getRedirectLoginHelper();
-//$permissions = ['email', 'user_likes']; // optional
-//$loginUrl = $helper->getLoginUrl('http://radiusdev.guestcompass.nl/callback.php', $permissions);
-//
-//echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-//exit;
-
-
 session_start();
-
 require '../login/Facebook/autoload.php';
 
 $fb = new Facebook\Facebook([
@@ -25,6 +7,15 @@ $fb = new Facebook\Facebook([
     'app_secret' => 'f7c94fe5f0f51cc9a04fc2512b5c58cd',
     'default_graph_version' => 'v2.8',
 ]);
+
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['email', 'user_likes']; // optional
+//$loginUrl = $helper->getLoginUrl('http://radiusdev.guestcompass.nl/callback.php', $permissions);
+$loginUrl = $helper->getLoginUrl('http://radiusdev.guestcompass.nl/fb_login.php', $permissions);
+
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+//exit;
+
 
 $helper = $fb->getRedirectLoginHelper();
 try {
